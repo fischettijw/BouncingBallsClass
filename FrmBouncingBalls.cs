a00bd8aa28a5e1eb7d7f8f591dfa28234aa55e41
Joseph Fischetti <fischettijw@gmail.com>
2020-07-20 14:25:07 -0400

    Add project files.

---
+++ FrmBouncingBalls.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBallsClass
+{
+    public partial class FrmBouncingBalls : Form
+    {
+        Timer Draw;
+        Color BackgroundColor;
+        Brush BackgroundBrush;
+        int x, y;
+        public FrmBouncingBalls()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmBouncingBalls_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            BackgroundColor = Color.AntiqueWhite;
+            BackgroundBrush = Brushes.AntiqueWhite;
+            x = 0;
+            y = 0;
+
+            this.BackColor = BackgroundColor;
+            this.Paint += FrmBouncingBalls_Paint;
+
+            Draw = new Timer();
+            Draw.Interval = 1;
+            Draw.Tick += Draw_Tick;
+            Draw.Enabled = true;
+        }
+
+        private void FrmBouncingBalls_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(BackgroundBrush, 0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height);
+            e.Graphics.FillRectangle(Brushes.Red, x, y, 50, 50);
+        }
+
+        private void Draw_Tick(object sender, EventArgs e)
+        {
+            x += 1;
+            y += 1;
+            this.Refresh();
+        }
+    }
+}
